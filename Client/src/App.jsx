4bc17b5b17d87e3185405d0d170edd7e6876ea09
Raju Achhami <achhami.raju55@gmail.com>
2024-09-24 16:38:17 +0545

    Header section added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Signin from "./components/Signin";
-import SignUp from "./components/SignUp";
-import Dashboard from "./components/Dashboard";
-import Projects from "./components/Projects";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
